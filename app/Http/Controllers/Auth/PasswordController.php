e22d9175a1376668fcdf36692ce66e3c53f93832
Lukman Hakim <lhakim885@gmail.com>
2024-06-09 10:28:01 +0800

    fix: admin layout profile edit page

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -24,6 +24,7 @@ class PasswordController extends Controller
             'password' => Hash::make($validated['password']),
         ]);
 
+        toastr()->success('Your password has been updated!');
         return back()->with('status', 'password-updated');
     }
 }
